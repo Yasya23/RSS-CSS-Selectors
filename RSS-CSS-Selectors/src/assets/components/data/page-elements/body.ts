14705fc542ba9e81aaf5d407a1f28478a1c74a9e
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-06-21 09:21:17 +0300

    refactor: view ts

---
+++ RSS-CSS-Selectors/src/assets/components/data/page-elements/body.ts
@@ -0,0 +1,14 @@
+import { Data } from '../../interfaces/data';
+
+const Body: Data = {
+  container: {
+    element: 'div',
+    classes: ['flex'],
+  },
+  wrapper: {
+    element: 'div',
+    classes: ['container', 'mx-auto', 'p-4'],
+  },
+};
+
+export { Body };
--- RSS-CSS-Selectors/src/assets/components/view/view.ts
+++ RSS-CSS-Selectors/src/assets/components/view/view.ts
@@ -1,30 +1,32 @@
+import { Body } from '../data/page-elements/body';
 import { Header } from './header/header';
 import { Main } from './main/main';
 import { Navigation } from './nav/nav';
 import { Footer } from './footer/footer';
+import { CreateHTMLElement } from './createHTMLelement';
 
 class View {
-  private container: HTMLDivElement;
-  private wrapper: HTMLDivElement;
+  private container: HTMLElement;
+  private wrapper: HTMLElement;
   private header: HTMLElement;
   private main: HTMLElement;
   private nav: HTMLElement;
   private footer: HTMLElement;
 
   constructor() {
+    const { container, wrapper } = Body;
+
     document.body.classList.add('font-sans', 'bg-gray-800');
 
-    this.container = document.createElement('div');
-    document.body.append(this.container);
-    this.container.classList.add('flex');
-
-    this.wrapper = document.createElement('div');
-    this.wrapper.classList.add('container', 'mx-auto', 'p-4');
+    this.container = new CreateHTMLElement(container).getElement();
+    this.wrapper = new CreateHTMLElement(wrapper).getElement();
     this.header = new Header().getElement();
     this.main = new Main().getElement();
     this.footer = new Footer().getElement();
-    this.wrapper.append(this.header, this.main, this.footer);
     this.nav = new Navigation().getElement();
+
+    document.body.append(this.container);
+    this.wrapper.append(this.header, this.main, this.footer);
     this.container.append(this.wrapper, this.nav);
   }
 
