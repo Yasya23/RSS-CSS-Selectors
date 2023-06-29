6be8cc43e7033bb415b4def53e791b0448641e06
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-06-29 14:29:06 +0300

    feat: type effect

--- RSS-CSS-Selectors/src/assets/components/view/main/editor-section/css-editor-field.ts
+++ RSS-CSS-Selectors/src/assets/components/view/main/editor-section/css-editor-field.ts
@@ -45,8 +45,22 @@ class EditorCss {
     this.setValue('fnjgn');
   }
 
-  private setValue(arg: string): void {
-    (this.inputField as HTMLInputElement).value = arg;
+  private setValue(answer: string): void {
+    const input = this.inputField as HTMLInputElement;
+    const value = answer;
+    let index = 0;
+    input.focus();
+    const typeEffect = setInterval(() => {
+      if (index >= value.length) {
+        clearInterval(typeEffect);
+        return;
+      }
+
+      const currentInputValue = input.value;
+      const nextChar = value.charAt(index);
+      input.value = currentInputValue + nextChar;
+      index++;
+    }, 200);
   }
 }
 
